af5c4c91031daa4b5543f3672e9643edf770056b
Vladislav Martyniuk <2alect@users.noreply.github.com>
2025-12-06 12:50:21 +0100

    Solved Day6

---
+++ AdventOfCode2025/Sources/Solvers/Day6SolverPart1.cs
@@ -0,0 +1,63 @@
+﻿using System.Numerics;
+
+using AdventOfCode2025.Utils;
+
+namespace AdventOfCode2025.Solvers;
+
+public class Day6SolverPart1 : IBaseSolver
+{
+	public string Solve(string input)
+	{
+		List<string> lines = input.Split('\n')
+			.Select(line => line.Trim())
+			.Where(line => line.Any())
+			.ToList();
+
+		List<List<int>> numGrid = lines.Take(lines.Count - 1)
+			.Select(ParseNumbers)
+			.ToList();
+
+		List<char> operators = ParseOperators(lines.Last());
+
+		BigInteger answer = 0;
+		for (int col = 0; col < operators.Count; col++)
+		{
+			BigInteger columnAnswer = numGrid[0][col];
+			for (int row = 1; row < numGrid.Count; row++)
+			{
+				if (operators[col] == '+')
+				{
+					columnAnswer += numGrid[row][col];
+				}
+				else if (operators[col] == '*')
+				{
+					columnAnswer *= numGrid[row][col];
+				}
+			}
+
+			answer += columnAnswer;
+		}
+
+		Log.Current.LogInformation($"Sum of all problems is: {answer}");
+
+		return answer.ToString();
+	}
+
+	private List<int> ParseNumbers(string line)
+	{
+		return line.Split(' ')
+			.Select(numStr => numStr.Trim())
+			.Where(numStr => numStr.Any())
+			.Select(int.Parse)
+			.ToList();
+	}
+
+	private List<char> ParseOperators(string line)
+	{
+		return line.Split(' ')
+			.Select(opStr => opStr.Trim())
+			.Where(opStr => opStr.Any())
+			.Select(opStr => opStr[0])
+			.ToList();
+	}
+}
---
+++ AdventOfCode2025/Sources/Solvers/Day6SolverPart2.cs
@@ -0,0 +1,120 @@
+﻿using System.Numerics;
+
+using AdventOfCode2025.Utils;
+
+namespace AdventOfCode2025.Solvers;
+
+public class Day6SolverPart2 : IBaseSolver
+{
+	public string Solve(string input)
+	{
+		(List<List<int>> problems, List<char> operators) = ParseInput(input);
+
+		BigInteger answer = BigInteger.Zero;
+
+		foreach ((List<int> numbers, char op) in problems.Zip(operators))
+		{
+			answer += CalcColumn(numbers, op);
+		}
+
+		Log.Current.LogInformation($"Sum of all problems is: {answer}");
+
+		return answer.ToString();
+	}
+
+	private (List<List<int>> problems, List<char> operators) ParseInput(string input)
+	{
+		List<string> lines = input.Split('\n')
+			.Where(line => line.Trim().Any())
+			.ToList();
+
+		List<string> numGrid = lines.Take(lines.Count - 1)
+			.ToList();
+
+		List<char> operators = ParseOperators(lines.Last());
+
+		return (ParseProblems(numGrid, operators.Count), operators);
+	}
+
+	private List<List<int>> ParseProblems(List<string> numGrid, int problemsCount)
+	{
+		int numbersInProblem = numGrid.Count;
+		List<List<int>> problems = new(problemsCount);
+		List<int> numbers = new List<int>(numbersInProblem);
+		bool prevHasNumber = true;
+
+		for (int col = 0; col < numGrid[0].Length; col++)
+		{
+			int num = 0;
+			bool hasNumber = false;
+			for (int row = 0; row < numGrid.Count; row++)
+			{
+				if (!char.IsDigit(numGrid[row][col])) 
+					continue;
+
+				hasNumber = true;
+				num = num * 10 + (numGrid[row][col] - '0');
+			}
+
+			if (hasNumber)
+			{
+				if (!prevHasNumber)
+				{
+					// Start a new problem
+					problems.Add(numbers);
+					numbers = new List<int>(numbersInProblem);
+				}
+
+				numbers.Add(num);
+			}
+
+			prevHasNumber = hasNumber;
+		}
+
+		problems.Add(numbers);
+
+		return problems;
+	}
+
+	private BigInteger CalcColumn(List<int> numbers, char op)
+	{
+		if (op == '+')
+			return CalcSum(numbers);
+		else
+			return CalcProduct(numbers);
+	}
+
+	private BigInteger CalcSum(List<int> numbers)
+	{
+		BigInteger sum = BigInteger.Zero;
+
+		foreach (int num in numbers)
+		{
+			sum += num;
+		}
+
+		return sum;
+	}
+
+	private BigInteger CalcProduct(List<int> numbers)
+	{
+		BigInteger sum = BigInteger.One;
+
+		foreach (int num in numbers)
+		{
+			sum *= num;
+		}
+
+		return sum;
+	}
+
+
+	private List<char> ParseOperators(string line)
+	{
+		return line.Split(' ')
+			.Select(opStr => opStr.Trim())
+			.Where(opStr => opStr.Any())
+			.Select(opStr => opStr[0])
+			.ToList();
+	}
+}
