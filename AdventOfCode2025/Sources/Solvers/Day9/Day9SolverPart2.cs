8145ec699c845489b62600cd0ea328edd147b50d
Vladislav Martyniuk <2alect@users.noreply.github.com>
2025-12-13 17:50:05 +0100

    Solution file for Day9 Part2

---
+++ AdventOfCode2025/Sources/Solvers/Day9/Day9SolverPart2.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode2025.Collections.Geometry;
+using AdventOfCode2025.Utils;
+using AdventOfCode2025.Solvers.Day9;
+
+namespace AdventOfCode2025.Solvers;
+
+public class Day9SolverPart2 : IBaseSolver
+{
+	public string Solve(string input)
+	{
+		Point2[] points = input.Split('\n')
+			.Select(line => line.Trim())
+			.Where(line => line.Any())
+			.Select(Common.PointFromStr)
+			.ToArray();
+
+		long answer = 0;
+
+		Log.Current.LogInformation($"Area of the largest rectangle is: {answer}");
+
+		return answer.ToString();
+	}
+}
