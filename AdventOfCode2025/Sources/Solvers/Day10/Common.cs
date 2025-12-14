777bccb97cf8e6d97fd58d0d5ba84580c80dec12
Vladislav Martyniuk <2alect@users.noreply.github.com>
2025-12-14 19:10:28 +0100

    Solved Day10 level 1

---
+++ AdventOfCode2025/Sources/Solvers/Day10/Common.cs
@@ -0,0 +1,71 @@
+﻿using AdventOfCode2025.Utils;
+
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2025.Solvers.Day10;
+
+public static class Common
+{
+	public static MachineManual[] ParseMachines(string input)
+	{
+		return input.Split("\n")
+			.Select(line => line.Trim())
+			.Where(line => line.Any())
+			.Select(ParseMachine)
+			.ToArray();
+	}
+
+	private static MachineManual ParseMachine(string line)
+	{
+		var mainRegex = new Regex(@"^\[(?<lights>[.#]+)\]\s+(?<buttons>(\([0-9,]+\)\s*)+)\{(?<weights>[0-9,]+)\}$", RegexOptions.Compiled);
+
+		Match m = mainRegex.Match(line);
+		if (!m.Success)
+			ExceptionHelper.ThrowException($"Error parse line: {line}");
+
+		// lights
+		string lights = m.Groups["lights"].Value;
+		int lightsMask = 0;
+		for (int i = 0; i < lights.Length; i++)
+		{
+			if (lights[i] == '#')
+				lightsMask |= 1 << i;
+		}
+
+		// buttons
+		Regex buttonRegex = new Regex(@"\((?<bits>[0-9,]+)\)");
+		int[][] buttonMasksBits = buttonRegex.Matches(m.Groups["buttons"].Value)
+			.Select(b =>
+				b.Groups["bits"].Value
+					.Split(',')
+					.Select(int.Parse)
+					.ToArray()
+			)
+			.ToArray();
+
+		int[] buttonMasks = new int[buttonMasksBits.Length];
+		for (int i = 0; i < buttonMasksBits.Length; i++)
+		{
+			int mask = 0;
+			foreach (int bit in buttonMasksBits[i])
+			{
+				mask |= 1 << bit;
+			}
+			buttonMasks[i] = mask;
+		}
+
+		return new MachineManual(lightsMask, buttonMasks);
+	}
+}
+
+public class MachineManual
+{
+	public int LightsMask { get; set; }
+	public int[] ButtonMasks { get; set; }
+
+	public MachineManual(int lightsMask, int[] buttonMasks)
+	{
+		LightsMask = lightsMask;
+		ButtonMasks = buttonMasks;
+	}
+}
---
+++ AdventOfCode2025/Sources/Solvers/Day10/Day10SolverPart1.cs
@@ -0,0 +1,41 @@
+﻿using AdventOfCode2025.Utils;
+using AdventOfCode2025.Solvers.Day10;
+
+namespace AdventOfCode2025.Solvers;
+
+public class Day10SolverPart1 : IBaseSolver
+{
+	public string Solve(string input)
+	{
+		MachineManual[] manuals = Common.ParseMachines(input);
+
+		long answer = manuals
+			.Select(SolveMachine)
+			.Sum();
+
+		Log.Current.LogInformation($"The fewest buttons pressed to configure all machines is: {answer}");
+
+		return answer.ToString();
+	}
+
+	private static int SolveMachine(MachineManual manual)
+	{
+		return SolveRecursive(0, manual.LightsMask, manual.ButtonMasks, 0, int.MaxValue, 0);
+	}
+
+	private static int SolveRecursive(int curMask, int targetMask, int[] masks, int togglesCount, int minToggles, int curInd)
+	{
+		if (curMask == targetMask)
+		{
+			return Math.Min(minToggles, togglesCount);
+		}
+
+		if (togglesCount >= minToggles || curInd >= masks.Length)
+			return minToggles;
+
+		int solveInclude = SolveRecursive(curMask ^ masks[curInd], targetMask, masks, togglesCount + 1, minToggles, curInd + 1);
+		int solveExcluse = SolveRecursive(curMask,					targetMask, masks, togglesCount,		minToggles, curInd + 1);
+
+		return Math.Min(solveInclude, solveExcluse);
+	}
+}
